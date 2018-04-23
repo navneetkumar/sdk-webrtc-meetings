1506c89abfcffc48892389dee2864aac9eac0e7e
Navneet Kumar <navneetk@bluejeans.com>
2018-04-23 07:37:19 +0530

    lib webpack

---
+++ consumer.js
@@ -0,0 +1,3 @@
+var lib =  require('./dist/webpack-numbers.js')
+console.log("CJS library = ", lib)
+console.log("CJS library result = ", lib.numToWord(1))
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'webpack-numbers.js',
+    library: 'webpackNumbers',
+    libraryTarget: 'commonjs2'
+  }
+};
