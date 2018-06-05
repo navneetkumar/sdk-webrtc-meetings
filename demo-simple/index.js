648a9f044f66802cf36cf3536c28505ce93783e7
Navneet Kumar <navneetk@bluejeans.com>
2018-06-05 22:38:05 +0530

    removing constants

--- demo-simple/index.js
+++ demo-simple/index.js
@@ -1,12 +1,8 @@
 console.log("BJN SDK Version = ", BJNClient.version)
-const meetingId = "600060006"
-const name = "Blue"
-
 const options = {
     localVideoEl: $("#localVideo")[0],
     remoteVideoEl: $("#remoteVideo")[0],
 }
-
 window.bjnClient = new BJNClient(options);
 
 bjnClient.on(bjnClient.events.media.callStateChanged, function(data) {
@@ -17,12 +13,10 @@ bjnClient.on(bjnClient.events.media.remoteStreamUpdated, function(data) {
     console.log("============> remote stream updated  = ", data)
 }) 
 
-bjnClient.init()
-
 $("#joinMeeting").click(function () {
     var meetingParams = {
-        numericMeetingId: meetingId,
-        displayName: name
+        numericMeetingId: "600060006",
+        displayName: "Blue"
     };
     bjnClient.joinMeeting(meetingParams);
 });
@@ -30,3 +24,5 @@ $("#joinMeeting").click(function () {
 $("#leaveMeeting").click(function () {
     bjnClient.leaveMeeting();
 });
+
+bjnClient.init()
